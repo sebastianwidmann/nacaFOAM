6bcbeaa270d39aa52b469395646ca70c127d9641
sebastianwidmann <sebastian.widmann@tum.de>
2023-05-23 12:10:15 +0200

    postProcessing.py: Updated algorithm.

--- postProcessing.py
+++ postProcessing.py
@@ -1,46 +1,70 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 # ----------------------------------------------------------------------------
 # Created By  : Sebastian Widmann
 # Institution : TU Munich, Department of Aerospace and Geodesy
 # Created Date: October 9, 2022
 # version ='1.0'
 # ---------------------------------------------------------------------------
-"""
-
-"""
-# ---------------------------------------------------------------------------
 import os
+import sys
 import pandas as pd
-import numpy as np
+import argparse
+from tqdm import tqdm
 
-cwd = os.getcwd()
 
-databaseDir = os.path.join(cwd, "database")
+def forceCoeffs_pp(src_dir: str):
+    if os.path.isfile(os.path.join(
+            os.path.dirname(src_dir), 'airfoilMNIST_forceCoeffs.csv')):
+        proceed = input('forceCoeffs data file exists. Do you want to '
+                        'overwrite? ')
+        if proceed.upper() == 'Y' or proceed.upper() == "YES":
+            pass
+        elif proceed.upper() == "N" or proceed.upper() == "NO":
+            sys.exit('Program aborted! File will not be overwritten.')
 
-databaseList = os.listdir(databaseDir)
-if 'nacaFOAM_Cl_Cd_Cm.csv' in databaseList:
-    databaseList.remove('nacaFOAM_Cl_Cd_Cm.csv')
+    dataset_list = os.listdir(src_dir)
 
-df = pd.DataFrame(columns=['naca', 'angle', 'mach', 'Cl', 'Cd', 'Cm'])
+    df = pd.DataFrame(
+        columns=['naca', 'angle', 'mach', 'Cl', 'Cd', 'Cm'])
 
-i = 0
-for case in databaseList:
-    caseDir = os.path.join(databaseDir, case)
-    caseName = case.split("_")
-    for file in os.listdir(caseDir):
-        if file.lower().endswith('.dat'):
-            fileDir = os.path.join(caseDir, file)
-            temp = pd.read_csv(fileDir, sep='\t', header=12, usecols=[1, 4, 7])
-            temp = temp.iloc[-100:]
-            temp = round(temp.mean(), 6)
+    i = 0
+    for sim in tqdm(dataset_list):
+        sim_config = sim[:-4]  # remove file ending
+        naca, angle, mach = sim_config.split("_", 2)[:3]
 
-            df.loc[i] = {'naca': caseName[0], 'angle': caseName[1], 'mach': caseName[2], 'Cl': temp[1], 'Cd': temp[0],
-                         'Cm': temp[2]}
+        file_dir = os.path.join(src_dir, sim)
 
-            i += 1
+        data = pd.read_csv(file_dir, sep='\t', header=12,
+                           usecols=[1, 4, 7])
+        data = round(data.iloc[-100:].mean(), 6)
 
-saveDir = os.path.join(databaseDir, "nacaFOAM_Cl_Cd_Cm.csv")
-df.to_csv(saveDir, sep='\t', index=None)
+        df.loc[i] = {'naca': naca,
+                     'angle': angle,
+                     'mach': mach,
+                     'Cl': data[1],
+                     'Cd': data[0],
+                     'Cm': data[2],
+                     }
 
-print('Post processing completed! Lift-drag data saved into: {}'.format(saveDir))
+        i += 1
+
+    save_dir = os.path.join(os.path.dirname(src_dir),
+                            'airfoilMNIST_forceCoeffs.csv')
+
+    df.to_csv(save_dir, sep='\t', index=False)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        prog='airfoilMNIST Postprocessing',
+        description='Generate .csv-file with mean force and moment coeffs'
+    )
+
+    parser.add_argument(
+        'src_dir',
+        type=str,
+        help='Specify load directory.'
+    )
+
+    args = parser.parse_args()
+
+    forceCoeffs_pp(args.src_dir)
